d20f1a454027da5878a31f14d00e8ab6d28e01c5
Junichi Ito <jit@sonicgarden.jp>
2023-07-02 09:53:09 +0900

    Simplify code

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -11,24 +11,15 @@ function main() {
   const startOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
   const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-  console.log(
-    `      ${rjust(date.getMonth() + 1, 2)}月 ${rjust(
-      date.getFullYear(),
-      4
-    )}     `
-  );
+  console.log(`      ${date.getMonth() + 1}月 ${date.getFullYear()}`);
   console.log("日 月 火 水 木 金 土");
   process.stdout.write(" ".repeat(startOfMonth.getDay() * 3));
   for (let i = 1; i <= endOfMonth.getDate(); i++) {
     let day = new Date(startOfMonth.getFullYear(), startOfMonth.getMonth(), i);
-    process.stdout.write(`${rjust(day.getDate().toString(), 2)} `);
+    process.stdout.write(`${day.getDate().toString().padStart(2)} `);
     if (day.getDay() == 6) {
       process.stdout.write("\n");
     }
   }
   process.stdout.write("\n\n");
-}
-
-function rjust(string, length) {
-  return " ".repeat(length - string.length) + string;
 }
