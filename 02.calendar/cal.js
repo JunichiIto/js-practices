e612765491b459d1cd2428e2fc7859099fb8cc8f
Junichi Ito <jit@sonicgarden.jp>
2023-07-02 09:54:38 +0900

    Refactoring

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,25 +1,21 @@
 import minimist from "minimist";
 
-main();
+const argv = minimist(process.argv.slice(2));
+const y = argv["y"];
+const m = argv["m"];
 
-function main() {
-  const argv = minimist(process.argv.slice(2));
-  const y = argv["y"];
-  const m = argv["m"];
+const date = new Date(y, m - 1, 1);
+const startOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-  const date = new Date(y, m - 1, 1);
-  const startOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-  const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-
-  console.log(`      ${date.getMonth() + 1}月 ${date.getFullYear()}`);
-  console.log("日 月 火 水 木 金 土");
-  process.stdout.write(" ".repeat(startOfMonth.getDay() * 3));
-  for (let i = 1; i <= endOfMonth.getDate(); i++) {
-    let day = new Date(startOfMonth.getFullYear(), startOfMonth.getMonth(), i);
-    process.stdout.write(`${day.getDate().toString().padStart(2)} `);
-    if (day.getDay() == 6) {
-      process.stdout.write("\n");
-    }
+console.log(`      ${date.getMonth() + 1}月 ${date.getFullYear()}`);
+console.log("日 月 火 水 木 金 土");
+process.stdout.write(" ".repeat(startOfMonth.getDay() * 3));
+for (let i = 1; i <= endOfMonth.getDate(); i++) {
+  const day = new Date(startOfMonth.getFullYear(), startOfMonth.getMonth(), i);
+  process.stdout.write(`${day.getDate().toString().padStart(2)} `);
+  if (day.getDay() === 6) {
+    process.stdout.write("\n");
   }
-  process.stdout.write("\n\n");
 }
+process.stdout.write("\n\n");
