182d8256aa77961aac283dffb49bf8877b45af7d
Junichi Ito <jit@sonicgarden.jp>
2023-07-02 10:31:37 +0900

    Split into methods

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,25 +1,40 @@
 import minimist from "minimist";
 
-const argv = minimist(process.argv.slice(2));
-const today = new Date();
-const y = argv.y ? argv.y : today.getFullYear();
-const m = argv.m ? argv.m : today.getMonth() + 1;
+main();
 
-console.log(`      ${m}月 ${y}`);
-console.log("日 月 火 水 木 金 土");
+function main() {
+  const argv = minimist(process.argv.slice(2));
+  const today = new Date();
+  const y = argv.y ? argv.y : today.getFullYear();
+  const m = argv.m ? argv.m - 1 : today.getMonth();
 
-const js_m = m - 1;
-const firstDay = new Date(y, js_m, 1).getDay();
-process.stdout.write(" ".repeat(firstDay * 3));
-const lastDate = new Date(y, js_m + 1, 0).getDate();
-const SATURDAY = 6;
-for (let i = 1; i <= lastDate; i++) {
-  process.stdout.write(i.toString().padStart(2));
-  const day = new Date(y, js_m, i).getDay();
-  if (day === SATURDAY) {
-    console.log();
-  } else {
-    process.stdout.write(" ");
-  }
+  printHeader(y, m);
+  printBody(y, m);
 }
-process.stdout.write("\n\n");
+
+function printHeader(y, m) {
+  console.log(`      ${m + 1}月 ${y}`);
+  console.log("日 月 火 水 木 金 土");
+}
+
+function printBody(y, m) {
+  printBlank(y, m);
+
+  const lastDate = new Date(y, m + 1, 0).getDate();
+  const SATURDAY = 6;
+  for (let d = 1; d <= lastDate; d++) {
+    process.stdout.write(d.toString().padStart(2));
+    const day = new Date(y, m, d).getDay();
+    if (day === SATURDAY) {
+      console.log();
+    } else {
+      process.stdout.write(" ");
+    }
+  }
+  process.stdout.write("\n\n");
+}
+
+function printBlank(y, m) {
+  const firstDay = new Date(y, m, 1).getDay();
+  process.stdout.write(" ".repeat(firstDay * 3));
+}
