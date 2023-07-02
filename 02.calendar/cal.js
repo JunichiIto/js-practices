d6fe65e15a78b8e5939ec29c19a69856d1f17bbb
Junichi Ito <jit@sonicgarden.jp>
2023-07-02 10:01:05 +0900

    Introduce js_m

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,17 +3,18 @@ import minimist from "minimist";
 const argv = minimist(process.argv.slice(2));
 const y = argv["y"];
 const m = argv["m"];
+const js_m = m - 1;
 
-const date = new Date(y, m - 1, 1);
+const date = new Date(y, js_m, 1);
 
-console.log(`      ${date.getMonth() + 1}月 ${date.getFullYear()}`);
+console.log(`      ${m}月 ${y}`);
 console.log("日 月 火 水 木 金 土");
 
-const endOfMonth = new Date(y, m, 0);
+const endOfMonth = new Date(y, js_m + 1, 0);
 process.stdout.write(" ".repeat(date.getDay() * 3));
 for (let i = 1; i <= endOfMonth.getDate(); i++) {
   process.stdout.write(`${i.toString().padStart(2)} `);
-  const day = new Date(y, m - 1, i);
+  const day = new Date(y, js_m, i);
   if (day.getDay() === 6) {
     process.stdout.write("\n");
   }
