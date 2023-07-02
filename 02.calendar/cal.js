ecefe8f316052c0f0a53aee05e543d0e834bce5e
Junichi Ito <jit@sonicgarden.jp>
2023-07-02 10:16:14 +0900

    Refactoring

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -3,20 +3,22 @@ import minimist from "minimist";
 const argv = minimist(process.argv.slice(2));
 const y = argv["y"];
 const m = argv["m"];
-const js_m = m - 1;
-
-const date = new Date(y, js_m, 1);
 
 console.log(`      ${m}月 ${y}`);
 console.log("日 月 火 水 木 金 土");
 
-const endOfMonth = new Date(y, js_m + 1, 0);
-process.stdout.write(" ".repeat(date.getDay() * 3));
-for (let i = 1; i <= endOfMonth.getDate(); i++) {
-  process.stdout.write(`${i.toString().padStart(2)} `);
-  const day = new Date(y, js_m, i);
-  if (day.getDay() === 6) {
-    process.stdout.write("\n");
+const js_m = m - 1;
+const firstDay = new Date(y, js_m, 1).getDay();
+process.stdout.write(" ".repeat(firstDay * 3));
+const lastDate = new Date(y, js_m + 1, 0).getDate();
+const SATURDAY = 6;
+for (let i = 1; i <= lastDate; i++) {
+  process.stdout.write(i.toString().padStart(2));
+  const day = new Date(y, js_m, i).getDay();
+  if (day === SATURDAY) {
+    console.log();
+  } else {
+    process.stdout.write(" ");
   }
 }
 process.stdout.write("\n\n");
